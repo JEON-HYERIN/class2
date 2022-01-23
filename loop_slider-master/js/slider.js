cba6c66c2edd5e3b9da492976d7425ea004314fe
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2022-01-23 13:08:35 +0900

    슬라이드

--- loop_slider-master/js/slider.js
+++ loop_slider-master/js/slider.js
@@ -6,14 +6,37 @@ $(선택자).prependTo('부모선택자');
 선택자를 잘라내기 해서 부모선택자 기준 제일 앞쪽으로 요소 배치
 */
 
-$('.prev').on('click', function (e) {
-  e.preventDefault();
+var frame = $('#slider1');
+var ul = frame.find('ul');
+var lis = ul.children('li');
+var prev = $('.prev');
+var next = $('.next');
+var speed = 2000;
+var enableClick = true;
 
-  $('#slider1 ul li').last().prependTo('#slider1 ul');
+prev.on('click', function (e) {
+  e.preventDefault();
+  prevSlide();
 });
 
-$('.next').on('click', function (e) {
+next.on('click', function (e) {
   e.preventDefault();
-
-  $('#slider1 ul li').first().appendTo('#slider1 ul');
+  if (enableClick) {
+    enableClick = false;
+    nextSlide();
+  }
 });
+
+function prevSlide() {
+  ul.stop().animate({left: '0%'}, speed, function () {
+    ul.children('li').last().prependTo(ul);
+    ul.css({left: '-100%'});
+  });
+}
+function nextSlide() {
+  ul.stop().animate({left: '-200%'}, speed, function () {
+    ul.children('li').first().appendTo(ul);
+    ul.css({left: '-100%'});
+    enableClick = true;
+  });
+}
